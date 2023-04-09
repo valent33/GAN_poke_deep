fe50b7ad17ee83c2cd9589fff842a8d62e577be3
valent33 <v.porchet@outlook.fr>
2023-04-09 15:07:40 -0400

    flask CGAN

--- flask_app/site.py
+++ flask_app/site.py
@@ -4,11 +4,12 @@ from flask_wtf import FlaskForm
 from wtforms import SubmitField
 from flask import render_template, request
 import os
+import numpy as np
 
 class PredictionForm(FlaskForm):
     submit = SubmitField('Predict')
 
-generator = tf.keras.models.load_model('generator_model_final.h5')
+generator = tf.keras.models.load_model('cgenerator_model_final.h5')
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret'
 
@@ -20,10 +21,13 @@ def index():
         # Get the checkbox values
         inputs = request.form.getlist('input[]')
         # Convert the values to floats
-        inputs = [float(i) for i in inputs]
-        print(inputs)
+        label = np.zeros(18)
+        for i in inputs:
+            label[int(i)-1] = 1
+        label = np.reshape(label, (1, 18))
+        print(label)
         # generate the image using the GAN model
-        image = generator.predict(tf.random.normal(shape=(1, 100)))
+        image = generator.predict([tf.random.normal(shape=(1, 100)), label])
         # save the image to a temporary file
         filename = 'temp.png'
         temp_file = os.path.join(app.config['UPLOAD_FOLDER'], filename).replace('\\', '/')
